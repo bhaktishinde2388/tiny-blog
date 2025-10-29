67868e7ad29650834ed592157c2fd034a446ad0b
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-10-29 16:27:54 +0530

    added the logic for showing the existanes of email

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,15 @@ import User from "../models/User.js";
 const postSignup = async (req,res)=>{
     const{name,email,password} = req.body; //taking info from req body
 
+    //existanes message showing
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.json({
+            success:false,
+            message:`this email ${email} already exists`
+        })
+    }
+
     const newUser = User({name,email,password})  //user model
 
     const savedUser = await newUser.save(); //save user
