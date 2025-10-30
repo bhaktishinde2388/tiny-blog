17ebb634e0844d3f5073bd26d7781b02c90acd4f
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-10-30 13:25:51 +0530

    login andsignup route add in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import AllBlog from './views/AllBlog.jsx';
 import NewBlog from './views/NewBlog.jsx';
 import EditBlog from './views/EditBlog.jsx';
 import ReadBlog from './views/ReadBlog.jsx';
+import Login from './views/Login.jsx';
+import SignUp from './views/SignUp.jsx';
 
 
 createRoot(document.getElementById('root')).render(
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')).render(
 <Route path="/newblog" element={<NewBlog />}/>
 <Route path="/edit/:id" element={<EditBlog />}/>
 <Route path="/readblog/:slug" element={<ReadBlog />}/>  
+<Route path="/login" element={<Login />}/>  
+<Route path="/signup" element={<SignUp />}/>  
 <Route path="*" element={<h1 className='text-center'>404 error ,path not match🫥</h1>}/>  
 </Routes>
 </BrowserRouter>
