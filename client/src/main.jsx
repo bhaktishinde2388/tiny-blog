c2d87d90d97eeaa5e93830870d9ba418b8d0b8db
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-10-30 13:17:56 +0530

    route setuped in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter,Routes,Route } from 'react-router'
 import './index.css'
-import App from './App.jsx'
+import AllBlog from './views/AllBlog.jsx';
+import NewBlog from './views/NewBlog.jsx';
+import EditBlog from './views/EditBlog.jsx';
+import ReadBlog from './views/ReadBlog.jsx';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<AllBlog/>}/>
+<Route path="/newblog" element={<NewBlog />}/>
+<Route path="/edit/:id" element={<EditBlog />}/>
+<Route path="/readblog/:slug" element={<ReadBlog />}/>  
+<Route path="*" element={<h1 className='text-center'>404 error ,path not match🫥</h1>}/>  
+</Routes>
+</BrowserRouter>
 )
